3ac05817fd2183d656abf29fe2160c0cdff81e95
Ellen Dunn <edunn227@gmail.com>
2018-11-06 11:06:01 -0600

    clean up

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -25,8 +25,7 @@ export default function reducer(state=initialState, action) {
       });
     }
 
-    else if (action.type === FETCH_FOOD_ITEMS_SUCCESS) {
-
+    else if (action.type === FETCH_FOOD_ITEMS_SUCCESS) 
       const {fooditems} = action.foodItems
       const fridgeItems = fooditems
             .filter(obj => obj.container === "fridge")
